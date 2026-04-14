81d1846862b632af592ccb4e6463db06c2c13e5d
mahmoudtarek10 <mahmoudtarekhassan3050@gmail.com>
2026-04-14 15:09:23 +0200

    push shifter

--- Design0/src/shifter.vhd
+++ Design0/src/shifter.vhd
@@ -15,14 +15,12 @@ end entity;
 architecture rtl of shifter is
 begin
 	
-<<<<<<< Updated upstream
-	-- TODO
-=======
+
 SHIFT_OUT <= SHIFT_INPUT(7 downto 0)&SHIFT_INPUT(15 downto 8) when SHIFT_CTRL = "1000" else	--ROR8
 			 SHIFT_INPUT(3 downto 0)&SHIFT_INPUT(15 downto 4) when SHIFT_CTRL = "1001" else	--ROR4
 			 SHIFT_INPUT(7 downto 0)&"00000000" when SHIFT_CTRL = "1010" else
 			 x"0000";	 
->>>>>>> Stashed changes
+
 	
 end architecture;
 	
