6e5f126d20b9e08c9d756279ff37bd7f96f9e52d
Ma7moudEGY <mahmoudfatahalla@gmail.com>
2026-04-15 01:12:03 +0200

    Init register file values; add register TB
    Include register_TB.vhd in Design0.adf project listings and initialize REG_FILE in register.vhd with predefined 16x16-bit hex values for registers 0..15. This provides a deterministic register state for simulation and testing while leaving any remaining entries as zeros.

--- Design0/src/register.vhd
+++ Design0/src/register.vhd
@@ -20,7 +20,25 @@ architecture rtl of register_file is
 
 --Declaration & Initialization Register_Files
 type reg_array is array(0 to 15) of std_logic_vector(15 downto 0);
-signal REG_FILE : reg_array := (others => (others => '0'));
+signal REG_FILE : reg_array := (
+	0 		=> x"0005",
+	1 		=> x"C404",
+	2 		=> x"9D22",
+	3 		=> x"1A50",
+	4 		=> x"1234",
+	5 		=> x"9876",
+	6 		=> x"ABCD",
+	7 		=> x"EF12",
+	8 		=> x"1AB2",
+	9 		=> x"9501",
+	10		=> x"FF00",
+	11		=> x"5500",
+	12		=> x"DFDF",
+	13		=> x"01CC",
+	14		=> x"54BA",
+	15		=> x"C000",
+	others 	=> (others => '0')
+);
 	
 begin 
 	
