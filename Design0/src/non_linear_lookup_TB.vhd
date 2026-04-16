97e0aa3f193b4c5d4b21c681361bd72000f0b52d
Mahmoud FathAllah <69058085+Ma7moudEGY@users.noreply.github.com>
2026-04-16 14:54:57 +0200

    Reformat non_linear_lookup_TB entity and architecture structure

--- Design0/src/non_linear_lookup_TB.vhd
+++ Design0/src/non_linear_lookup_TB.vhd
@@ -1,19 +1,16 @@
 library ieee;  
 use ieee.std_logic_1164.all;
 	 
-
-
-  entity lockupTB is  
-  end entity;
+entity ‎non_linear_lookup_TB is  
+end entity;
   
-  architecture behavior of lockupTB is
+architecture behavior of non_linear_lookup_TB is
   
-  --signals
-  signal LUT_IN,LUT_OUT: std_logic_vector(7 downto 0);
+--signals
+signal LUT_IN,LUT_OUT: std_logic_vector(7 downto 0);
  
-  begin
-	  
-	  
+begin
+	
   -- component linkage 	  
   u: entity non_linear_lookup 
 	 port map (
@@ -21,38 +18,24 @@ use ieee.std_logic_1164.all;
 	   LUT_OUT => LUT_OUT
 	   );
 	   
-	      
-
   -- Stimulus process
   process
-  begin
-        
+  begin  
         -- Test Cases
         LUT_IN <= "00000000"; 
         wait for 10 ns;
 
-        
         LUT_IN <= "00010001";
         wait for 10 ns;
-
-        
+  
         LUT_IN <= "10101010";
         wait for 10 ns;
 
-        
         LUT_IN <= "11111111";
         wait for 10 ns;
-
-        
-        LUT_IN <= "01010101";
-        wait;
-		
-		
-		
- end process;
  
-
-end; 
-
-	  
-	  
+        LUT_IN <= "01010101";
+        wait;	
+		
+   end process;
+end architecture;	  
