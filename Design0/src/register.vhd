21be22b520d498f913b315c6a1c6ffc80bf8eb11
Youssef Reda <youssefreda866@gmail.com>
2026-05-03 16:00:08 +0300

    modified Register-File to Reset all registers when RST = '1'

--- Design0/src/register.vhd
+++ Design0/src/register.vhd
@@ -20,32 +20,47 @@ architecture rtl of register_file is
 
 --Declaration & Initialization Register_Files
 type reg_array is array(0 to 15) of std_logic_vector(15 downto 0);
-signal REG_FILE : reg_array := (others => (others => '0'));
+signal REG_FILE : reg_array := (
+	0 		=> x"0005",
+	1 		=> x"C404",
+	2 		=> x"9D22",
+	3 		=> x"1A50",
+	4 		=> x"1234",
+	5 		=> x"9876",
+	6 		=> x"ABCD",
+	7 		=> x"EF12",
+	8 		=> x"1AB2",
+	9 		=> x"9501",
+	10		=> x"FF00",
+	11		=> x"5500",
+	12		=> x"DFDF",
+	13		=> x"01CC",
+	14		=> x"54BA",
+	15		=> x"C000",
+	others 	=> (others => '0')
+);
 	
 begin 
 	
 	--Write_Operation
 	write_process: process(Clk)
-	begin
-		if(rising_edge(Clk)) then  
-			if (En = '1')then 	  -- Write when En = '1'
-				REG_FILE(to_integer(unsigned(Rd))) <= RES;	
+	begin	
+		if (Rst = '1') then       -- Reset Register-Files when RST = '1'
+			REG_FILE <= (others => (others => '0')); 	
+		elsif(rising_edge(Clk)) then  
+			if (En = '1') then 	  -- Write when En = '1'
+				REG_FILE(to_integer(unsigned(Rd))) <= RES;
+				-- For debugging purposes
+				report "Wrote " & integer'image(to_integer(unsigned(RES))) & " to " & integer'image(to_integer(unsigned(Rd)));
 			end if;	
 		end if;	
 	end process;
-	
-	--Read_Operation
-	read_process: process(Clk)
-	begin
-		if(rising_edge(Clk)) then  
-			if(Rst = '1') then    
-				SRCa <= x"0000";
-				SRCb <= x"0000";
-			else 
-				SRCa <= REG_FILE(to_integer(unsigned(Ra)));	
-				SRCb <= REG_FILE(to_integer(unsigned(Rb)));
-			end if;
-		end if;	
-	end process;
 
+	-- Asynchronous Read
+	SRCa <= REG_FILE(to_integer(unsigned(Ra))) when Rst = '0' else
+			x"0000";
+		
+	SRCb <= REG_FILE(to_integer(unsigned(Rb))) when Rst = '0' else
+			x"0000";
+					
 end architecture rtl;
